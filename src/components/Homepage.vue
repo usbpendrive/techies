8d94c2f1d98da24543841cc398e1051fe2f6defd
usbpendrive <ferry.fernando@gmail.com>
2018-06-22 14:24:21 +0700

    Initialize new Vue application.

---
+++ src/components/Homepage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Homepage</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Homepage'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
