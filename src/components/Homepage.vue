0964628cbf63e5e98d2f0be53f010182ee9f9137
usbpendrive <ferry.fernando@gmail.com>
2018-06-22 14:52:46 +0700

    Add axios package. Consuming hackernews API.

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,12 +1,42 @@
 <template>
   <div>
     <h2>Homepage</h2>
+    <div v-for="story in stories" :key="story.id">
+      <h2>{{ story.data.title }}</h2>
+      <p>Type: {{ story.data.type }}</p>
+      <p>Link: {{ story.data.url }}</p>
+      <p>Score: {{ story.data.score }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Homepage'
+  name: 'Homepage',
+  data: function () {
+    return {
+      err: '',
+      stories: []
+    }
+  },
+  created: function () {
+    axios.get('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then((result) => {
+        this.results = result.data.slice(0, 10)
+        this.results.forEach(element => {
+          axios.get('https://hacker-news.firebaseio.com/v0/item/' + element + '.json')
+            .then((result) => {
+              this.stories.push(result)
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+        })
+      })
+      .catch((err) => { this.err = err })
+  }
 }
 </script>
 
